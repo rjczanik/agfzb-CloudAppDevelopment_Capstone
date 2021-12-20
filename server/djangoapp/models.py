9a76bbf427a86f9a4a5bb468455229a6c2c5eaa5
Robert J Czanik <robert.czanik@gmail.com>
2021-12-20 04:34:55 +0100

    Finished first week's lab session

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=100)
 
+    def __str__(self):
+        return "Make: " + self.name + "," + "\n Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -21,7 +26,82 @@ from django.utils.timezone import now
 # - __str__ method to print a car make object
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'Suv'
+    WAGON = 'Wagon'
+    STYLE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+        (SUV, 'Suv')
+    ]
 
+    make = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField()
+    model_name = models.CharField(null=False, max_length=30)
+    style = models.CharField(
+        null=False,
+        max_length=20,
+        choices=STYLE_CHOICES,
+        default=SUV
+    )
+    year = models.DateField(default=now)
+
+    def __str__(self):
+        return "Make: " + str(self.make) + "," + "\n DealerId: " + str(self.dealer_id) + "," + "\n Model: " + self.model_name + "," + "\n Style: " + self.style + "," + "\n Year: " + str(self.year)
+
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, state, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer st(state)
+        self.st = st
+        # Dealer state
+        self.state = state
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+
+class DealerReview:
+
+    #def __init__(self,car_make,car_model,car_year,dealership,id,name,purchase,purchase_date,review,sentiment):
+    def __init__(self, dealership, name, purchase, review):
+        #self.car_make=car_make
+        self.car_make = ""
+        #self.car_model=car_model
+        self.car_model = ""
+        #self.car_year=car_year
+        self.car_year = 0
+        self.dealership = dealership
+        #self.id=id
+        self.id = 0
+        self.name = name
+        self.purchase = purchase
+        #self.purchase_date=purchase_date
+        self.purchase_date = ""
+        self.review = review
+        #self.sentiment=sentiment
+        self.sentiment = "default"
+
+    def __str__(self):
+        return "Review: " + self.review
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
+from .models import CarMake, CarModel, CarDealer
 # from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_request, get_dealer_reviews_id_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -20,35 +22,185 @@ logger = logging.getLogger(__name__)
 # Create an `about` view to render a static about page
 # def about(request):
 # ...
-
+def about(request):
+    return render(request, 'djangoapp/about.html')
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
 
+
+def contact(request):
+    return render(request, 'djangoapp/contact_us.html')
+
+
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Try to check if provide credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If user is valid, call login method to login current user
+            login(request, user)
+            return redirect('djangoapp:index')  # home page redirect
+        else:
+            # If not, return to login page again
+            #messages.error(request,'Invalid User Name or Password')
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to course list view
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
 
+
+def registration_request(request):
+    context = {}
+    # If it is a GET request, just render the registration page
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            # Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            # Login the user and redirect to course list page
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            return render(request, 'djangoapp/registration.html', context)
+
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+
+
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
+        #url = "your-cloud-function-domain/dealerships/dealer-get"
+        url = "https://c2ba3bfa.us-south.apigw.appdomain.cloud/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        #save to list to render in template
+        dealer_list = []
+        for dealer in dealerships:
+            dealership = {}
+            dealership["id"] = dealer.id
+            dealership["full_name"] = dealer.full_name
+            dealership["city"] = dealer.city
+            dealership["state"] = dealer.state
+            dealership["zip"] = dealer.zip
+            dealership["address"] = dealer.address
+            print(dealership)
+            dealer_list.append(dealership)
+        print("dealer_list: ", dealer_list)
+        context = {"dealer_list": dealer_list}
+        print(context)
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        #return HttpResponse(dealer_names)
         return render(request, 'djangoapp/index.html', context)
-
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
 
+
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        parameters = {"dealership": dealer_id}
+        url = "https://c2ba3bfa.us-south.apigw.appdomain.cloud/api/review"
+        reviews = get_dealer_reviews_id_from_cf(url, kwargs=parameters)
+        print("get_dealer_details() --> (reviews):", reviews)
+        #review_cat=' '.join([review.review for review in reviews])
+        #review_cat2=' '.join([review.sentiment for review in reviews])
+        #return HttpResponse(review_cat)
+        analyzed_reviews = []
+        for review in reviews:
+            individual_review = {}
+            individual_review["dealership"] = review.dealership
+            individual_review["car_make"] = review.car_make
+            individual_review["car_model"] = review.car_model
+            individual_review["car_year"] = review.car_year
+            individual_review["review"] = review.review
+            individual_review["sentiment"] = review.sentiment
+            analyzed_reviews.append(individual_review)
+        context = {"analyzed_reviews": analyzed_reviews}
+        print("context: ", context)
+        return render(request, 'djangoapp/dealer_details.html', context)
+        #for review in reviews:
+        #message1=review.review
+        #message2=review.sentiment
+        #message=message1 + " (" + message2 + ") "
+        #temp.append(message)
+        #reviews_analyzed=" ".join(temp)
+        #return HttpResponse(reviews_analyzed)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
 
+
+def add_review(request, dealer_id):
+    if request.user.is_authenticated:
+        new_review = dict()
+        new_review["car_make"] = "Ford"
+        new_review["car_model"] = "MustangGT"
+        new_review["car_year"] = 2021
+        new_review["dealership"] = dealer_id
+        new_review["id"] = 11
+        new_review["name"] = "Neddy Speddy"
+        new_review["purchase"] = True
+        new_review["purchase_date"] = "12/14/21"
+        new_review["review"] = "Dreaming of Speed in my Wake."
+
+        review_payload = {}
+        review_payload["review"] = new_review
+        #review_payload=new_review
+        #convert python dictionary to json object
+        #review_payload_json=json.dumps(review_payload)
+        #print("review_payload_json: ",review_payload_json)
+
+        review_post_url = "https://c2ba3bfa.us-south.apigw.appdomain.cloud/api/review"
+        parameters = {"dealership": dealer_id}
+        print("parameters: ", parameters)
+        post_response = post_request(
+            review_post_url, review_payload, **parameters)
+        print(post_response)
+        return JsonResponse(post_response)
+    else:
+        print("Unauthenticated User Please Log in to Submit Review")
